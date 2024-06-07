2ab3b5805c1555fcd5b6a54c3e38395116c0796d
omkargutal <117467302+omkargutal@users.noreply.github.com>
2024-06-07 13:04:13 +0530

    Add files via upload

---
+++ codewithharry yt learning basic/omkar53.js
@@ -0,0 +1,24 @@
+console.log("omkar 53 tutorial");
+
+function greet(name, salutation){
+    console.log(salutation =" Good Morning"+ name )
+    console.log(name +"is a good boy")
+}
+
+
+let name =" omkar ";
+let name1 =" abhishek ";
+let name2 =" aniket";
+let name3 =" sagar  ";
+// console.log(name + "is a good boy" )
+greet(name ,);
+greet(name1);
+greet(name2);
+greet(name3);
+
+function sum(a,b,c){
+    let d = a+ b + c ;
+    return d;
+}
+let returnval = sum (1,2,3)
+console.log(returnval)
